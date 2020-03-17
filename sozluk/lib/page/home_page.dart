5fcc85b0b3daf34440de9e6f99325422ffc2aba8
right7ctrl <//.3cfe170c.21/>
2020-03-18 00:20:02 +0300

    genel homepage şablonu ve searchbox

--- sozluk/lib/page/home_page.dart
+++ sozluk/lib/page/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sozluk/util/app_constant.dart';
+
+import '../util/app_widget.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -11,15 +14,56 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: AppConstant.colorPageBg,
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Home'),
-        ],
-      )),
+        child: Stack(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: size.height * .35,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    image: DecorationImage(
+                      image: AssetImage('assets/bg.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Center(
+                    child: SvgPicture.asset(
+                      AppConstant.svgLogo,
+                      height: 40,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text('Content here'),
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              top: size.height * .35 - 32,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Material(
+                  elevation: 4,
+                  borderRadius: BorderRadius.circular(8),
+                  child: AppWidget.getSearchBox(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
