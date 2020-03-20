e8bfcc6adc9d5357076b9823e47a35889311766b
onatcipli <onatcipli@outlook.com>
2020-03-20 21:43:02 +0300

    HomeNavigator indentation added

--- sozluk/lib/page/home/home_navigator.dart
+++ sozluk/lib/page/home/home_navigator.dart
@@ -12,29 +12,50 @@ class HomeNavigator extends StatefulWidget {
 class _HomeNavigatorState extends State<HomeNavigator> {
   int _currentPage = 1;
 
-  List<Widget> _pages = [HistoryPage(), HomePage(), Center(child: Text('Todo'))];
+  List<Widget> _pages = [
+    HistoryPage(),
+    HomePage(),
+    Center(
+      child: Text(
+        'Todo',
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: ConvexAppBar(
+        // height causes layout overflow on some devices
+        // height: 56,
         color: AppConstant.colorParagraph2,
         backgroundColor: Colors.white,
         activeColor: AppConstant.colorPrimary,
+        style: TabStyle.fixedCircle,
         elevation: 0.5,
-        //height causes layout overflow on some devies
-        //height: 56,
+
+        initialActiveIndex: _currentPage,
+
         onTap: (int val) {
           if (val == _currentPage) return;
           setState(() {
             _currentPage = val;
           });
         },
-        initialActiveIndex: _currentPage,
-        style: TabStyle.fixedCircle,
+
         items: <TabItem>[
-          TabItem(icon: Icons.history, title: ''),
-          TabItem(icon: Icons.search, title: ''),
-          TabItem(icon: Icons.bookmark_border, title: ''),
+          TabItem(
+            icon: Icons.history,
+            title: '',
+          ),
+          TabItem(
+            icon: Icons.search,
+            title: '',
+          ),
+          TabItem(
+            icon: Icons.bookmark_border,
+            title: '',
+          ),
         ],
       ),
       body: _pages[_currentPage],
